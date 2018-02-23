8b9af19b8da4ac64cebc5751bd8447498b0f3240
Tordu5 <da_mekk@web.de>
2018-02-23 20:12:10 +0100

    minimal project- TestEngine issue
    The minimal project where the Testengine find no Tests.
    Small changes fix this problem like making out of
    "group 'project'"
    "group 'com.project'"

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import io.vertx.junit5.VertxExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+public class Test {
+
+
+    @org.junit.jupiter.api.Test
+    @ExtendWith(VertxExtension.class)
+    void Test() {
+    }
+}
